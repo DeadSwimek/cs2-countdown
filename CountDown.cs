c8e5c38da2e2a8d5092c9b5e584dab363a7a41f6
DeadSwim <66752697+DeadSwimek@users.noreply.github.com>
2023-12-02 13:15:12 +0100

    Update CountDown.cs

--- CountDown.cs
+++ CountDown.cs
@@ -29,12 +29,13 @@ public partial class CountDown : BasePlugin
     public override string ModuleAuthor => "DeadSwim";
     public override string ModuleDescription => "Simple Special rounds.";
     public override string ModuleVersion => "V. 1.0.0";
-    private static readonly int?[] IsVIP = new int?[65];
 
 
 
     public float Time;
+    public string Text;
     public bool Countdown_enable;
+    public bool Countdown_enable_text;
 
 
     public override void Load(bool hotReload)
@@ -42,6 +43,8 @@ public partial class CountDown : BasePlugin
         RegisterListener<Listeners.OnMapStart>(name =>
         {
             Countdown_enable = false;
+            Countdown_enable_text = false;
+            Text = "";
             Time = 0;
 
         });
@@ -60,7 +63,16 @@ public partial class CountDown : BasePlugin
                 {
                     client.PrintToCenterHtml(
                     $"<font color='gray'>----</font> <font class='fontSize-l' color='green'>COUNTDOWN</font><font color='gray'>----</font><br>" +
-                    $"<font color='gray'>►</font> <font class='fontSize-m' color='green'>[{Time}]</font> <font color='gray'>◄</font><br>" +
+                    $"<font color='gray'>►</font> <font class='fontSize-m' color='green'>[{Time} seconds]</font> <font color='gray'>◄</font><br>" +
+                    $"<font color='gray'>----</font> <font class='fontSize-l' color='green'>COUNTDOWN</font><font color='gray'>----</font><br>"
+                    );
+                }
+                if (Countdown_enable_text)
+                {
+                    client.PrintToCenterHtml(
+                    $"<font color='gray'>----</font> <font class='fontSize-l' color='green'>COUNTDOWN</font><font color='gray'>----</font><br>" +
+                    $"<font color='gray'>►</font> <font class='fontSize-l' color='gold'>[{Text}]</font><font color='gray'>◄</font><br>"+
+                    $"<font color='gray'>►</font> <font class='fontSize-m' color='green'>[{Time} seconds]</font> <font color='gray'>◄</font><br>" +
                     $"<font color='gray'>----</font> <font class='fontSize-l' color='green'>COUNTDOWN</font><font color='gray'>----</font><br>"
                     );
                 }
@@ -96,9 +108,45 @@ public partial class CountDown : BasePlugin
         Countdown_enable = true;
         var timer = AddTimer(1.0f, () =>
         {
-            if (Time == 0)
+            if (Time == 0.0)
             {
                 Countdown_enable = false;
+                return;
+            }
+
+            Time = Time - 1.0f;
+        }, TimerFlags.REPEAT);
+    }
+    [ConsoleCommand("css_countdown_text", "Start countdown with text")]
+    public void CommandStartTextCountDown(CCSPlayerController? player, CommandInfo info)
+    {
+        if (!AdminManager.PlayerHasPermissions(player, "@css/root"))
+        {
+            player.PrintToChat($" [{ChatColors.Lime}CountDown{ChatColors.Default}] You are not admin..");
+            return;
+        }
+        var TimeSec = info.ArgByIndex(1);
+        var Text_var = info.ArgByIndex(2);
+
+        if (TimeSec == null || TimeSec == "" || !IsInt(TimeSec))
+        {
+            player.PrintToChat($" [{ChatColors.Lime}CountDown{ChatColors.Default}] You must use that {ChatColors.Lime}/countdown_text <TIME_SECONDS> <TEXT>{ChatColors.Default}.");
+            return;
+        }
+        if (Text_var == null || Text_var == "")
+        {
+            player.PrintToChat($" [{ChatColors.Lime}CountDown{ChatColors.Default}] You must use that {ChatColors.Lime}/countdown_text <TIME_SECONDS> <TEXT>{ChatColors.Default}.");
+            return;
+        }
+        var time_convert = Convert.ToInt32(TimeSec);
+        Time = time_convert;
+        Text = Text_var;
+        Countdown_enable_text = true;
+        var timer = AddTimer(1.0f, () =>
+        {
+            if (Time == 0.0)
+            {
+                Countdown_enable_text = false;
                 return;
             }
 
